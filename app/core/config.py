ece6494464c81d6df9881cd750c650569dbb0dcc
Alexander Butakov <asbutakov@gmail.com>
2022-05-29 15:20:20 +0300

    added env.template file

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     first_superuser_password: Optional[str] = None
 
     class Config:
-        env_file = '.env'
+        env_file = 'env.template'
 
 
 settings = Settings()
