5d49b60e95b54ff72e8084de46afeaaa86abffe9
Alexander Butakov <asbutakov@gmail.com>
2022-05-29 13:14:21 +0300

    create user table

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (charity_project_router,
-                               donation_router)
+                               donation_router,
+                               user_router)
 
 
 main_router = APIRouter()
@@ -15,3 +16,4 @@ main_router.include_router(
     prefix='/donation',
     tags=['Donations']
 )
+main_router.include_router(user_router)
