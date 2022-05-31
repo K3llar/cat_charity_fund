11d19f33992971f49c1e2fd803744e7621fca47d
Alexander Butakov <asbutakov@gmail.com>
2022-05-31 17:24:54 +0300

    disable create_superuser function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
-from app.core.init_db import create_first_superuser
+# from app.core.init_db import create_first_superuser
 from app.api.routers import main_router
 
 app = FastAPI(title=settings.app_title,
@@ -9,7 +9,7 @@ app = FastAPI(title=settings.app_title,
 
 app.include_router(main_router)
 
-
-@app.on_event('startup')
-async def startup():
-    await create_first_superuser()
+# Отключено из-за тестов на платформе
+# @app.on_event('startup')
+# async def startup():
+#     await create_first_superuser()
